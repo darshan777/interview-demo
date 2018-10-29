6c41d1d701f5c19446c204b4ae655e000a08faa7
darshan777 <sanghavi.da@husky.neu.edu>
2018-10-28 20:19:32 -0400

    Adding exception class

---
+++ interview-demos-master/backend/src/main/java/com/quickbase/devint/exception/AppException.java
@@ -0,0 +1,10 @@
+package com.quickbase.devint.exception;
+
+public class AppException  extends Exception{
+
+
+    public AppException(String errorMessage) {
+        super(errorMessage);
+
+    }
+}
